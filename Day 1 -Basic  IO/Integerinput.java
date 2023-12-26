07e4fb199ad53623022079fb9b3856710220e227
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-12-26 11:20:02 +0530

    Add files via upload

---
+++ Day 1 -Basic  IO/Integerinput.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class getinput {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int a=obj.nextInt();
+        System.out.println("Entered Integer: "+a);
+    }
+}
+
+/*
+Enter an integer:
+10
+Entered Integer: 10
+*/
