c5e3dbf016a34f00e63d9b423dac3fd354538309
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-12-26 11:06:43 +0530

    Add files via upload

---
+++ Day 1 -Basic  IO/Average.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class getinput {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter two floating numbers: ");
+        float a=obj.nextFloat();
+        float b=obj.nextFloat();
+	float avg=(a+b)/2;
+        System.out.printf("Average: %.2f",avg);
+    }
+}
+
+
+/*
+Enter two floating numbers: 
+10.3222
+15.4133
+Average: 12.87
+*/
