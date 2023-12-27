ca54cd7af3bc854bca8dfd523c9287b5f93ace3c
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-12-27 11:50:10 +0530

    permutations.java

--- Day 2 - Nested loops/permutations.java
+++ Day 2 - Nested loops/permutations.java
@@ -1,71 +1,81 @@
 import java.util.*;
-public class Permutations{
-    public static void permutation(int[] arr,int n) {
-		List<List<Integer>> res=new ArrayList<>();
-		res.add(new ArrayList<>());
-		for(int i=0;i<n;i++) {
-			List<List<Integer>> arr2=new ArrayList<>();
-			for(int j=0;j<=i;j++){
-				for(List<Integer> item:res) {
-					List<Integer> temp=new ArrayList<>(item);
-					temp.add(j,arr[i]);
-					arr2.add(temp);
-				}
-			}
-			res=arr2;
-		}
-		for(List<Integer> i:res)
-		   System.out.println(i);
-	}
-
+public class permutations{
     public static void main(String[] args) {
         Scanner obj=new Scanner(System.in);
-		System.out.println("Enter no. of elements: ");
-		int n=obj.nextInt();
-		int arr[]=new int[n];
-		System.out.println("Enter the elements of the array: ");
-		for(int i=0;i<n;i++)
-		   arr[i]=obj.nextInt();
-	    permutation(arr,n);
+        System.out.print("Enter no. of elements:");
+        int n=obj.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i=0;i<n;i++) 
+            arr[i]=obj.nextInt();
+        System.out.println("Permutations: ");
+        permutation(arr,n);
+    }
+    public static void permutation(int[] arr,int n) {
+        int res[]=new int[n];
+        System.out.println(Arrays.toString(arr));
+        int i=0;
+        while(i<n){
+            if(res[i]<i){
+                if(i%2==0) 
+                    swap(arr,0,i);
+                else 
+                    swap(arr,res[i],i);
+                System.out.println(Arrays.toString(arr));
+                res[i]++;
+                i=0;
+            } 
+            else{
+                res[i]=0;
+                i++;
+            }
+        }
+    }
+    public static void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 }
 
-
 /*
-Enter no. of elements: 
-1
-Enter the elements of the array: 
-1
+Enter no. of elements: 1
+Enter the elements of the array: 1
 [1]
 
+Enter no. of elements:2
+Enter the elements of the array:1 4
+Permutations: 
+[1, 4]
+[4, 1]
 
-Enter no. of elements: 
-4
-Enter the elements of the array: 
-1 2 3 4
-[4, 3, 2, 1]
-[4, 3, 1, 2]
-[4, 2, 3, 1]
-[4, 1, 3, 2]
+
+Enter no. of elements:4
+Enter the elements of the array: 1 2 3 4
+Permutations: 
+[1, 2, 3, 4]
+[2, 1, 3, 4]
+[3, 1, 2, 4]
+[1, 3, 2, 4]
+[2, 3, 1, 4]
+[3, 2, 1, 4]
 [4, 2, 1, 3]
-[4, 1, 2, 3]
-[3, 4, 2, 1]
-[3, 4, 1, 2]
-[2, 4, 3, 1]
-[1, 4, 3, 2]
 [2, 4, 1, 3]
 [1, 4, 2, 3]
-[3, 2, 4, 1]
-[3, 1, 4, 2]
-[2, 3, 4, 1]
-[1, 3, 4, 2]
+[4, 1, 2, 3]
 [2, 1, 4, 3]
 [1, 2, 4, 3]
-[3, 2, 1, 4]
-[3, 1, 2, 4]
-[2, 3, 1, 4]
-[1, 3, 2, 4]
-[2, 1, 3, 4]
-[1, 2, 3, 4]
+[1, 3, 4, 2]
+[3, 1, 4, 2]
+[4, 1, 3, 2]
+[1, 4, 3, 2]
+[3, 4, 1, 2]
+[4, 3, 1, 2]
+[4, 3, 2, 1]
+[3, 4, 2, 1]
+[2, 4, 3, 1]
+[4, 2, 3, 1]
+[3, 2, 4, 1]
+[2, 3, 4, 1]
 
 */
