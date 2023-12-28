957e8d8ac0e84cb87c88a5c11e32ec999069dd84
SanthoshiniKS <111353954+SanthoshiniKS@users.noreply.github.com>
2023-12-28 12:47:49 +0530

    PalPrime.java

--- Day 3 - Classes and Objects/PalPrime.java
+++ Day 3 - Classes and Objects/PalPrime.java
@@ -1,16 +1,8 @@
 import java.util.Scanner;
 public class PalPrime {
-    public PalPrime(int num){
-            boolean ispalindrome=check_palindrome(num);
-            boolean isprime=check_prime(num);
-            if(ispalindrome && isprime)
-                System.out.println("Number "+num+" is " +"PalPrime");
-            else if(ispalindrome)
-                System.out.println("Number "+num+" is " +"Palindrome");
-            else if(isprime)
-                System.out.println("Number "+num+" is " +"Prime");
-            else
-                System.out.println("Number "+num+" is " +"neither Palindrome nor Prime and not PalPrime");
+    
+    public PalPrime(int num,String message){
+        System.out.println("Number "+num+" is "+message);
     }
     public static void main(String[] args) 
     {
@@ -21,11 +13,13 @@ public class PalPrime {
         System.out.println("Enter the elements of the array: ");
         for (int i=0;i<n;i++) 
             arr[i]=obj.nextInt();
+        String a="";
         for (int i=0;i<n;i++){
-           new PalPrime(arr[i]);
+           String message=PalPrime.display(arr[i]);
+           new PalPrime(arr[i],message);
         }
-        
     }
+
     public static boolean check_palindrome(int num) {
         int rev=0,x=num;
         while(num!=0){
@@ -35,7 +29,7 @@ public class PalPrime {
         }
         return x==rev;
     }
-
+    
     public static boolean check_prime(int num) {
         if (num<=1) 
             return false;
@@ -45,4 +39,41 @@ public class PalPrime {
         }
         return true;
     }
+    
+    public static String display(int num){
+            boolean ispalindrome=check_palindrome(num);
+            boolean isprime=check_prime(num);
+            if(ispalindrome && isprime)
+                return "PalPrime";
+            else if(ispalindrome)
+                return "Palindrome";
+            else if(isprime)
+                return "Prime";
+            else
+                return "neither Palindrome nor Prime and not PalPrime";
+    }
 }
+
+
+/*
+Enter the size of the array: 5
+Enter the elements of the array: 
+1 34543 565 727 10099
+Number 1 is Palindrome
+Number 34543 is PalPrime
+Number 565 is Palindrome
+Number 727 is PalPrime
+Number 10099 is Prime
+
+Enter the size of the array: 
+7
+Enter the elements of the array: 
+13 2114 644 211 567 8999 2
+Number 13 is Prime
+Number 2114 is neither Palindrome nor Prime and not PalPrime
+Number 644 is neither Palindrome nor Prime and not PalPrime
+Number 211 is Prime
+Number 567 is neither Palindrome nor Prime and not PalPrime
+Number 8999 is Prime
+Number 2 is PalPrime
+*/
